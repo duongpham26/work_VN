f88dc52ecbd3a1206e849d2b389b6560acf17707
duongpham26 <duong.pham2617@gmail.com>
2025-02-24 16:46:39 +0700

    updateBy add

--- src/main/java/com/duongpham26/demo/entity/Company.java
+++ src/main/java/com/duongpham26/demo/entity/Company.java
@@ -1,12 +1,18 @@
 package com.duongpham26.demo.entity;
 
+import java.lang.StackWalker.Option;
 import java.time.Instant;
+import java.util.Optional;
+
+import com.duongpham26.demo.util.SecurityUtil;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
@@ -31,6 +37,7 @@ public class Company {
 
    private String logo;
 
+   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Bangkok")
    private Instant createdAt; // sử dụng life cycle của spring để cập nhậtnhật
 
    private Instant updatedAt;
@@ -38,4 +45,12 @@ public class Company {
    private String createdBy;
 
    private String updatedBy;
+
+   @PrePersist // callback method
+   public void handleBeforeCreatedAt() {
+      Optional<String> userInfo = SecurityUtil.getCurrentUserLogin();
+      this.createdBy = userInfo.isPresent() ? userInfo.get() : "";
+      this.createdAt = Instant.now(); // lưu theo chuẩn ISO đã cấu hình
+      // Lỗi mối giới mặc định lưu là múi giờ 0; => chỉnh thành múi giờ 7 (VN)
+   }
 }
