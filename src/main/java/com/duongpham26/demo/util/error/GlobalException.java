b0c6b38079a8d218299e32936c486276cd04d197
duongpham26 <duong.pham2617@gmail.com>
2025-02-25 20:51:41 +0700

    add not found excepiton

--- src/main/java/com/duongpham26/demo/util/error/GlobalException.java
+++ src/main/java/com/duongpham26/demo/util/error/GlobalException.java
@@ -12,6 +12,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import com.duongpham26.demo.entity.RestResponse;
 
@@ -44,5 +45,14 @@ public class GlobalException {
       res.setMessage(errors.size() > 1 ? errors : errors.get(0));
 
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
+   }
+
+   @ExceptionHandler(value = { NoResourceFoundException.class })
+   public ResponseEntity<RestResponse<Object>> handleNotFoundException(Exception ex) {
+      RestResponse<Object> res = new RestResponse<Object>();
+      res.setStatusCode(HttpStatus.NOT_FOUND.value());
+      res.setError(ex.getMessage());
+      res.setMessage("404 Not Found.");
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(res);
    }
 }
